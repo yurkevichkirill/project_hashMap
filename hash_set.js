4fb9549daccd13ae8a497cc3bca33284f5f9a3ca
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2025-08-20 21:33:56 +0300

    Create Hash data structure using Linked List

---
+++ hash_set.js
@@ -0,0 +1,140 @@
+import { LinkedListToSet } from "./linked_list_to_set.js";
+
+export class HashSet{
+    constructor(loadFactor = 0.75, capacity = 16){
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        this.buckets = [];
+        for(let i = 0; i < this.capacity; i++){
+            this.buckets[i] = new LinkedListToSet();
+        }
+    }
+
+    hash(key) {
+        let hashCode = 0;
+            
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    set(key){
+        const index = this.hash(key);
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if(this.buckets[index].size() === 0 || !this.buckets[index].contains(key)){
+            this.buckets[index].append(key);
+        } 
+        //else if(this.buckets[index].contains(key)){
+
+        // } else {
+        //     this.buckets[index].append([key, value]);
+        // }
+        if(this.length() > this.capacity * this.loadFactor){
+            for(let i = this.capacity; i < this.capacity * 2; i++){
+                this.buckets[i] = new LinkedListToSet();
+            }
+            this.capacity *= 2;
+            const copy = this.keys().slice();
+            this.clear();
+            copy.forEach((key) => {
+                this.set(key);
+            });
+        }
+    }
+
+    length(){
+        let total = 0;
+        this.buckets.forEach((bucket) => {
+            total += bucket.size();
+        });
+        return total;
+    }
+
+    // get(key){
+    //     const index = this.hash(key);
+    //     if (index < 0 || index >= this.capacity) {
+    //         throw new Error("Trying to access index out of bounds");
+    //     }
+    //     if(this.buckets[index].contains(key)){
+    //         const listIndex = this.buckets[index].find(key);
+    //         return this.buckets[index].at(listIndex).data[1];
+    //     }
+    //     else{
+    //         return null;
+    //     }
+    // }
+
+    has(key){
+        const index = this.hash(key);
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if(this.buckets[index].contains(key)){
+            return true;
+        }
+        else{
+            return false;
+        }                
+    }
+
+    remove(key){
+        const index = this.hash(key);
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if(this.buckets[index].contains(key)){
+            const listIndex = this.buckets[index].find(key);
+            this.buckets[index].removeAt(listIndex);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    clear(){
+        this.buckets.forEach((bucket) => {
+            while(bucket.size() > 0){
+                bucket.pop();
+            }
+        })
+    }
+
+    keys(){
+        let keys = [];
+        this.buckets.forEach((bucket) => {
+            let listIndex = 0;
+            while(listIndex < bucket.size()){
+                keys.push(bucket.at(listIndex++).data);
+            }
+        });
+        return keys;
+    }
+
+    // values(){
+    //     let values = [];
+    //     this.buckets.forEach((bucket) => {
+    //         let listIndex = 0;
+    //         while(listIndex < bucket.size()){
+    //             values.push(bucket.at(listIndex++).data[1]);
+    //         }
+    //     });
+    //     return values;    
+    // }
+
+    // entries(){
+    //     let entries = [];
+    //     this.buckets.forEach((bucket) => {
+    //         let listIndex = 0;
+    //         while(listIndex < bucket.size()){
+    //             entries.push(bucket.at(listIndex++).data);
+    //         }
+    //     });
+    //     return entries;           
+    // }
+
+}
--- main.js
+++ main.js
@@ -1,42 +1,82 @@
 import { HashMap } from "./hash_map.js";
+import { HashSet } from "./hash_set.js";
 
-const test = new HashMap();
+// const test = new HashMap();
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+// test.set('apple', 'red')
+// test.set('banana', 'yellow')
+// test.set('carrot', 'orange')
+// test.set('dog', 'brown')
+// test.set('elephant', 'gray')
+// test.set('frog', 'green')
+// test.set('grape', 'purple')
+// test.set('hat', 'black')
+// test.set('ice cream', 'white')
+// test.set('jacket', 'blue')
+// test.set('kite', 'pink')
+// test.set('lion', 'golden')
+
+// console.log(test.length());
+// console.log(test.capacity);
+
+// test.set('moon', 'silver');
+// test.set('grae', 'purple')
+// test.set('ha', 'black')
+// test.set('ie cream', 'white')
+// test.set('acket', 'blue')
+// test.set('kte', 'pink')
+// test.set('ion', 'golden')
+
+
+// console.log(test.length());
+// console.log(test.capacity);
+
+// console.log(test.keys());
+// console.log(test.values());
+// console.log(test.entries());
+
+// //test.clear();
+// console.log(test.get('kte'));
+// console.log(test.get('grae'));
+
+// test.remove('grae');
+// console.log(test.entries());
+
+const test = new HashSet();
+
+test.set('apple')
+test.set('banana')
+test.set('carrot')
+test.set('dog')
+test.set('elephant')
+test.set('frog')
+test.set('grape')
+test.set('hat')
+test.set('ice cream')
+test.set('jacket')
+test.set('kite')
+test.set('lion')
 
 console.log(test.length());
 console.log(test.capacity);
 
-test.set('moon', 'silver');
-test.set('grae', 'purple')
-test.set('ha', 'black')
-test.set('ie cream', 'white')
-test.set('acket', 'blue')
-test.set('kte', 'pink')
-test.set('ion', 'golden')
+test.set('moon');
+test.set('grae')
+test.set('ha')
+test.set('ie cream')
+test.set('acket')
+test.set('kte')
+test.set('ion')
 
 
 console.log(test.length());
 console.log(test.capacity);
 
 console.log(test.keys());
-console.log(test.values());
-console.log(test.entries());
 
 //test.clear();
-console.log(test.get('kte'));
-console.log(test.get('grae'));
+console.log(test.has('kte'));
+console.log(test.has('grae'));
+console.log(test.has('graefff'));
 
 test.remove('grae');
-console.log(test.entries());
